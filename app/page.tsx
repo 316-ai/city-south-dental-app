863f2fa65b38b82b78f1a22908a3ada55ccf9c18
edwinjosekdk <edwinjosekdk@gmail.com>
2024-04-30 04:06:19 +0530

    feat: added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-"use client";
-
-import { Readex_Pro } from "next/font/google";
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import Banner from "./components/banner";
 import Search from "./components/search";
 
-const readex_pro = Readex_Pro({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
-      <style jsx global>{`
-        * {
-          font-family: ${readex_pro.style.fontFamily};
-        }
-      `}</style>
-
       <Navbar />
       <section className="content">
         <Banner />
