95a67ec22e37f81853a97b6c9263324809524087
edwinjosekdk <edwinjosekdk@gmail.com>
2024-05-17 13:25:52 +0530

    feat: blog detail page

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -17,10 +17,19 @@ export default function Blogsdetail() {
         <section className="blogs px-3 py-7 lg:py-12 bg-white">
           <div className="lg:container mx-auto">
             <div>
+              <Image
+                src="/happy-customer.jpeg"
+                alt="dots"
+                width={1000}
+                height={0}
+                className="w-full h-[360px] object-cover rounded-3xl shadow mb-6"
+              />
+
               <span className="border px-3 py-1 rounded-lg text-sm text-gray-400">
                 12 August, 2023
               </span>
-              <h1 className="text-xl font-semibold text-gray-700 my-4">
+              <h6 className="text-sm text-sky-600 mt-4">mini caption</h6>
+              <h1 className="text-xl font-semibold text-gray-700 mt-2 mb-4">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
                 cum amet perferendis commodi. Voluptate, quos.
               </h1>
@@ -30,13 +39,43 @@ export default function Blogsdetail() {
                 tenetur maxime repellat quae ducimus?
               </h2>
 
-              <Image
-                src="/happy-customer.jpeg"
-                alt="dots"
-                width={1000}
-                height={0}
-                className="w-full h-[400px] object-cover rounded-3xl shadow mb-5"
-              />
+              <div className="grid grid-cols-8 gap-12 md:gap-8 my-6">
+                <div className="col-span-4 md:col-span-3">
+                  <Image
+                    src="/healthy-smile.jpeg"
+                    alt="healthy smile"
+                    className="h-full object-cover shadow rounded-3xl"
+                    width={1500}
+                    height={1000}
+                  />
+                </div>
+                <div className="col-span-8 md:col-span-5 flex flex-col justify-center">
+                  <h2 className="text-2xl text-sky-900 font-semibold mb-3">
+                    Building Healthy Smiles And Happy Lives
+                  </h2>
+                  <p className="text-sm text-gray-600 font-thin mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur voluptatem fugit impedit eveniet! Atque, quas
+                    voluptates quae culpa laboriosam pariatur quos fuga,
+                    deserunt, ipsum laborum rem officiis vitae eos. Error Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                    veritatis cupiditate ut quibusdam rerum reprehenderit
+                    cumque.
+                  </p>
+                  <p className="text-sm text-sky-900 mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Blanditiis ullam recusandae natus voluptatibus corporis
+                    aperiam doloribus unde necessitatibus in. Perspiciatis saepe
+                    ullam sed earum? Similique, explicabo recusandae tempore
+                    veniam minima ea Lorem ipsum dolor, sit amet consectetur
+                    adipisicing elit. Ab a totam hic ducimus! Ullam itaque
+                    perspiciatis, nobis ipsam velit commodi? Lorem ipsum dolor,
+                    sit amet consectetur adipisicing elit. Dolore totam quae
+                    quasi et porro ex odio accusamus, ut incidunt omnis sapiente
+                    ipsam laudantium qui necessitatibus. Laborum?
+                  </p>
+                </div>
+              </div>
 
               <p className="text font-light text-gray-700 mb-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
