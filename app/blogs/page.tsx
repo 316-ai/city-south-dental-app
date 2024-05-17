fbbc1548525ba7e1fda6b1646247fff999144114
edwinjosekdk <edwinjosekdk@gmail.com>
2024-05-17 12:18:28 +0530

    feat: blog cards

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Pagebanner from "../components/pagebanner";
+import Blogcard from "../components/blogcard";
 
 export default function Blogs() {
   return (
@@ -9,6 +10,17 @@ export default function Blogs() {
       <Navbar />
       <section className="content">
         <Pagebanner name="Blogs" />
+
+        <div className="px-3 py-7 lg:py-12 bg-gray-50">
+          <div className="lg:container mx-auto">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-8">
+              <Blogcard />
+              <Blogcard />
+              <Blogcard />
+              <Blogcard />
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </>
---
+++ app/components/blogcard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import FeatherIcon from "feather-icons-react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+const Blogcard = () => {
+  return (
+    <>
+      <div className="transition-all rounded-3xl overflow-hidden border shadow hover:shadow-lg">
+        <Image
+          src="/emergency-dental-care.jpeg"
+          alt="emergency-dental-care"
+          width={600}
+          height={0}
+          className="w-full h-60 object-cover"
+        />
+
+        <div className="py-3 px-4">
+          <span className="border px-3 py-1 rounded text-sm text-gray-400">
+            12 August, 2023
+          </span>
+
+          <h1 className="line-clamp-1 font-semibold text-gray-700 mt-3">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime cum
+            amet perferendis commodi. Voluptate, quos.
+          </h1>
+
+          <h6 className="line-clamp-2 text-sm text-gray-500 font-light mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            temporibus rerum consequatur soluta iure accusamus, nulla expedita
+            tempora velit nobis.
+          </h6>
+
+          <Link
+            href="/"
+            className="text-sm flex items-center uppercase text-sky-600 hover:underline mt-3"
+          >
+            <span>Read more</span>
+            <span>
+              <FeatherIcon icon="arrow-right" className="size-4 ml-2" />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blogcard;
