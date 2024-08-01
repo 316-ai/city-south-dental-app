bbf6b5b49658c8d96454156e77ef60f520e25a02
Jithin Vinod <72181381+jithinvinod72@users.noreply.github.com>
2024-08-01 12:12:52 -0400

    Update page.tsx

--- app/cdcp/page.tsx
+++ app/cdcp/page.tsx
@@ -30,7 +30,7 @@ export default function Cdcp() {
                   What is the Canadian Dental Care Plan (CDCP)?
                 </h1>
 
-                <h2 className="text-md font-thin text-gray-900 mb-4">
+                <h2 className="text-md font-normal text-gray-900 mb-4">
                   The Canadian Dental Care Plan (CDCP) is a federal government
                   program intended to reduce financial barriers to oral health
                   care. The program will provide coverage of the cost of dental
@@ -41,7 +41,7 @@ export default function Cdcp() {
                 <p className="text-gray-600 mb-4">
                   Our City South Dental Clinic is now accepting CDCP patients*.
                 </p>
-                <p className="text-gray-600 mb-4 text-sm font-thin">
+                <p className="text-gray-600 mb-4 text-sm font-normal">
                   For more information, please visit{" "}
                   <a
                     href="https://www.citysouthdental.com"
@@ -187,25 +187,25 @@ export default function Cdcp() {
               <h6 className="text-sm text-sky-600">
                 CDCP Program Acceptance and Enrollment
               </h6>
-              <p className="text-gray-600 font-thin mb-4">
+              <p className="text-gray-600 font-normal mb-4">
                 Coverage will begin in May 2024 for the earliest applicants.
                 Patients MUST have a member card to receive coverage through the
                 CDCP.
               </p>
-              <p className="text-gray-600 font-thin mb-4">
+              <p className="text-gray-600 font-normal mb-4">
                 The welcome package from Sun Life will include the date a
                 patient can start seeing an oral health provider. Appointments
                 can only be scheduled on or after that date for the eligible
                 oral health care services to be covered under the plan.
               </p>
-              <p className="text-gray-600 mb-4 font-thin">
+              <p className="text-gray-600 mb-4 font-normal">
                 Patient CDCP start dates will depend on:
               </p>
-              <ul className="list-disc list-inside text-gray-600 mb-4 font-thin">
+              <ul className="list-disc list-inside text-gray-600 mb-4 font-normal">
                 <li>when their application was received and</li>
                 <li>when their enrolment is completed</li>
               </ul>
-              <p className="text-gray-600 font-thin mb-4">
+              <p className="text-gray-600 font-normal mb-4">
                 The first group that will be eligible to visit an oral health
                 provider will be as early as May 2024, starting with seniors.
               </p>
@@ -243,7 +243,7 @@ export default function Cdcp() {
                     <span className="mr-2">✅</span> Sedation dentistry services
                   </li>
                 </ul>
-                <p className="text-sm text-gray-600 font-thin mb-5">
+                <p className="text-sm text-gray-600 font-normal mb-5">
                   Some oral health procedures will require preauthorization.
                   This process involves obtaining prior approval, based on the
                   recommendation of the oral health provider, before commencing
@@ -258,11 +258,11 @@ export default function Cdcp() {
                 <h6 className="font-semibold font-bold text-sky-900 mb-4">
                   How much will the CDCP cover?
                 </h6>
-                <p className="text-gray-600 font-thin mb-2">
+                <p className="text-gray-600 font-normal mb-2">
                   Depending on your family’s income, you might need to pay a
                   small part of the bill. This amount is paid to your dentist.
                 </p>
-                <p className="text-gray-600 font-thin mb-2">
+                <p className="text-gray-600 font-normal mb-2">
                   The CDCP will reimburse a percentage of the cost, based on the
                   established CDCP fee guide and your adjusted family net
                   income. You may have a co-payment based on your adjusted
@@ -332,9 +332,9 @@ export default function Cdcp() {
                     </tbody>
                   </table>
                 </div>
-                <p className="text-sm text-gray-600 font-thin mb-2 mt-2">
+                <p className="text-sm text-gray-600 font-normal mb-2 mt-2">
                   * Additional fees – The CDCP fees may not be the same as what
-                  providers charge and only pays for services covered within the
+                  providers charge and only pays for services covered winormal the
                   plan at the established CDCP rates. These rates are not the
                   same as provincial and territorial fee guides that dentists
                   usually utilize as reference to establish their usual and
@@ -359,17 +359,17 @@ export default function Cdcp() {
                 <h6 className="font-semibold font-bold text-sky-900 mb-4">
                   How to apply for CDCP coverage?
                 </h6>
-                <p className="text-gray-600 font-thin mb-4">
+                <p className="text-gray-600 font-normal mb-4">
                   Invitation letters have been sent in phases by age group
                   between December 2023 and March 2024 to all potentially
                   eligible seniors who are 70 years of age or older, welcoming
                   them to apply for the CDCP.
                 </p>
-                <p className="text-gray-600 font-thin mb-4">
+                <p className="text-gray-600 font-normal mb-4">
                   New* seniors aged 65 and older are now eligible to apply for
                   the CDCP online without an invitation letter.
                 </p>
-                <p className="text-gray-600 font-thin mb-4">
+                <p className="text-gray-600 font-normal mb-4">
                   For more information, please visit{" "}
                   <a
                     href="https://www.canada.ca/dental"
